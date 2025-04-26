a93959189b87bb929d2fd089c5074ee6e85732d8
fatihberber1 <fatihberber.res@gmail.com>
2025-04-26 13:52:41 +0300

    api kilidi kaldırıldı

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from routes.auth_routes import router as auth_router
 from routes.vote_routes import router as vote_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    title="E-Seçim Sistemi",
+    title="Blockchain E-Seçim Sistemi",
     description="7 bölgeye ayrılmış blockchain tabanlı güvenli elektronik oylama sistemi",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # herkes erişebilir
+    allow_methods=["*"],  # GET, POST, PUT, DELETE vs hepsi izinli
+    allow_headers=["*"],  # Content-Type, Authorization vs izinli
 )
 
 app.include_router(auth_router, prefix="", tags=["Authentication"])
